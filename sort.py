7b6bb3e6be354133c53711e1a22fb388220509bf
liuchao <17621726396@163.com>
2018-05-17 23:48:17 +0800

    change sort.py

---
+++ sort.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python3
+words = ['cat', 'window', 'defenestrate']
+for w in words:
+    print(w,  len(w))
+
+a = ['Mary', 'had', 'a', 'little', 'lamb']
+for i in range(len(a)):
+    print(i, a[i])
